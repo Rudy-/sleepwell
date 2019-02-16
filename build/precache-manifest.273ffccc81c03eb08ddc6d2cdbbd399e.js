a4f354149ea908f9ccc5166e2d435e1274078960
Rudy- <rudy59239@hotmail.fr>
2019-02-16 19:29:40 +0100

    Creation of the build

---
+++ build/precache-manifest.273ffccc81c03eb08ddc6d2cdbbd399e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1b28f4414561819d6fb8",
+    "url": "/static/js/main.1b28f441.chunk.js"
+  },
+  {
+    "revision": "a86d43958676f572e2c4",
+    "url": "/static/js/2.a86d4395.chunk.js"
+  },
+  {
+    "revision": "1b28f4414561819d6fb8",
+    "url": "/static/css/main.8f619d77.chunk.css"
+  },
+  {
+    "revision": "a86d43958676f572e2c4",
+    "url": "/static/css/2.e7c49faf.chunk.css"
+  },
+  {
+    "revision": "44e3b31ae731f851d598667bc07088e5",
+    "url": "/index.html"
+  }
+];
